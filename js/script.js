9072b1c6be134b273c79aa130915d4feb01c43e9
Raymond Matsuki <20329752+rmatsuki@users.noreply.github.com.>
2025-04-08 18:40:05 -0700

    Create 16x16 grid

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@
     The Odin Project: Etech-A-Sketch Game
 */
 
+function createGrid()
+{
+    const gridContainer = document.querySelector(".container");
+    for (let i = 1; i <= 256; i++)
+    {
+        // justify-content is x-axis in row; align-items is y-axis in row.
+        let square = document.createElement("div");
+        square.className = "square";
+        gridContainer.appendChild(square);
+    }
+
+    return gridContainer;
+}
+
+function main()
+{
+    // Create 256 div elements, then store into var.
+    // Append the var into document.body.
+    gridContainer = createGrid();
+    document.body.appendChild(gridContainer);
+}
+
+main();
